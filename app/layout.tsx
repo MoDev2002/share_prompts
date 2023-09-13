29a8ca292631a4cb8768ba2b57499f0bc40af8d8
Mohamed El Masry <91676840+MoDev2002@users.noreply.github.com>
2023-09-13 20:10:21 +0300

    adding favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,9 @@ function RootLayout({ children }: { children: React.ReactNode })
 {
   return (
     <html lang='en'>
+      <head>
+        <link rel='icon' href='/favicon.ico' />
+      </head>
       <body>
         <Provider>
           <div className='main'>
